9f0a6b33ede880b679f4548d063f71a14ece1f06
henrykh <henryhowes@gmail.com>
2015-03-02 18:05:12 -0800

    changes user/profile rel

--- imager/imager/models.py
+++ imager/imager/models.py
@@ -1,18 +1,19 @@
 from django.db import models
-from django.conf import settings
+from django.contrib.auth.models import User
 
 
-# class ImagerProfile(models.Manager):
-#     def active():
-#         from django.db import connection
-#         cursor = connection.cursor()
+class ImagerProfile(models.Manager):
+    def active():
+        from django.db import connection
+        cursor = connection.cursor()
 
 
 class profile(models.Model):
-    user = models.OneToOneField(settings.AUTH_USER_MODEL)
     # objects = ImagerProfile()
 
-    picture = models.FileField()
+    associated_user = models.OneToOneField(User)
+
+    picture = models.FileField(blank=True)
     picture_privacy = models.BooleanField(default=True)
 
     phone_number = models.CharField(max_length=20)
