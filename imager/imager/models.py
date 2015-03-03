5659bc868b46745c9da0cff6e722f7578794d88a
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-02 17:28:51 -0800

    makemigrations and migrate.

--- imager/imager/models.py
+++ imager/imager/models.py
@@ -2,20 +2,20 @@ from django.db import models
 from django.conf import settings
 
 
-class ImagerProfile(models.Manager):
-    def active():
-        from django.db import connection
-        cursor = connection.cursor()
+# class ImagerProfile(models.Manager):
+#     def active():
+#         from django.db import connection
+#         cursor = connection.cursor()
 
 
 class profile(models.Model):
     user = models.OneToOneField(settings.AUTH_USER_MODEL)
-    objects = ImagerProfile()
+    # objects = ImagerProfile()
 
     picture = models.FileField()
     picture_privacy = models.BooleanField()
 
-    phone_number = models.CharField()
+    phone_number = models.CharField(max_length=20)
     phone_privacy = models.BooleanField()
 
     birthday = models.DateField()
