2c518b07e416befb1f5ddba8d5f2380c5e8511ab
henrykh <henryhowes@gmail.com>
2015-03-02 19:35:14 -0800

    adds post_save to watch newly created users

--- imager/imager/models.py
+++ imager/imager/models.py
@@ -1,25 +1,19 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.db.models.signals import post_save
 
 
-class ImagerProfile(models.Manager):
-    def active(self):
-        qs = self.get_queryset()
-        return qs.filter(associated_user__is_active=True)
-
-
-class profile(models.Model):
-    objects = ImagerProfile()
+class ImagerProfile(models.Model):
 
     associated_user = models.OneToOneField(User)
 
     picture = models.FileField(blank=True)
     picture_privacy = models.BooleanField(default=True)
 
-    phone_number = models.CharField(max_length=20)
+    phone_number = models.CharField(max_length=20, blank=True)
     phone_privacy = models.BooleanField(default=True)
 
-    birthday = models.DateField()
+    birthday = models.DateField(blank=True)
     birthday_privacy = models.BooleanField(default=True)
 
     name_privacy = models.BooleanField(default=True)
@@ -30,3 +24,15 @@ class profile(models.Model):
 
     def is_active(self):
         return self.associated_user.is_active
+
+    @classmethod
+    def active(self):
+        qs = self.get_queryset()
+        return qs.filter(associated_user__is_active=True)
+
+    def create_profile(sender, **kwargs):
+        ip = ImagerProfile(associated_user=kwargs["instance"])
+        ip.save()
+
+
+    post_save.connect(create_profile, sender=User)
