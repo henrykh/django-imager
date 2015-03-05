643a9ed617e8dc20078de246663965496823fcf7
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-04 16:48:51 -0800

    add photo model.

--- imager_images/models.py
+++ imager_images/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Album(models.Model):
+    pass
+
+
+class Photo(models.Model):
+    user = models.ForeignKey(User, related_name='photos')
+    albums = models.ManyToManyField(Album, related_name='photos')
+
+    title = models.CharField(max_length=50, blank=True)
+    description = models.TextField(blank=True)
+    date_uploaded = models.DateField(null=True, blank=True)
+    date_modified = models.DateField(null=True, blank=True)
+    date_published = models.DateField(null=True, blank=True)
+
+    PRIVATE = 'pvt'
+    SHARED = 'shd'
+    PUBLIC = 'pub'
+    PHOTO_PRIVACY_OPTIONS = (
+        (PRIVATE, 'Private'),
+        (SHARED, 'Shared'),
+        (PUBLIC, 'Public'),
+    )
+
+    published = models.CharField(max_length=3,
+                                 choices=PHOTO_PRIVACY_OPTIONS,
+                                 default=PRIVATE)
