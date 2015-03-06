2cacc684e04c08c1ca813a3acaa4218517e9e39b
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-05 22:46:27 -0800

    ImagerProfile inline in User working.

--- imager_user/admin.py
+++ imager_user/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from models import ImagerProfile
+from django.contrib.auth.admin import UserAdmin
+from django.contrib.auth.models import User
 
 
 class ProfileInline(admin.StackedInline):
@@ -15,17 +17,18 @@ class ProfileAdmin(admin.ModelAdmin):
                     )
 
 
-class UserAdmin(admin.ModelAdmin):
+class UserAdmin(UserAdmin):
     list_display = ('username',
                     'first_name',
                     'last_name',
-                    'email'
-                    'is_staff'
+                    'email',
+                    'is_staff',
                     'is_active',
                     'date_joined'
                     )
 
     inlines = [ProfileInline, ]
 
-
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
 admin.site.register(ImagerProfile, ProfileAdmin)
