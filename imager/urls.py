a61ceb2f4ef1ad92c0290d045e407f1971bdd41e
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-06 12:09:21 -0800

    working thumbnails.

--- imager/urls.py
+++ imager/urls.py
@@ -1,10 +1,15 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+import settings
+from django.conf import settings as dcs
+from django.conf.urls.static import static
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'imager.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
+                       # Examples:
+                       # url(r'^$', 'imager.views.home', name='home'),
+                       # url(r'^blog/', include('blog.urls')),
+                       url(r'^admin/', include(admin.site.urls)),
+                       )
 
-    url(r'^admin/', include(admin.site.urls)),
-)
+if settings.DEBUG:
+    urlpatterns += static(dcs.MEDIA_URL, document_root=dcs.MEDIA_ROOT)
