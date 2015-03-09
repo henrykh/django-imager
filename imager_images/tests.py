4c5389cca22492c2cef35eefc2459ad1fcb83030
henrykh <henryhowes@gmail.com>
2015-03-09 14:50:13 -0700

    adds test for photo model

--- imager_images/tests.py
+++ imager_images/tests.py
@@ -5,6 +5,8 @@ from django.contrib.auth.models import User
 from tempfile import NamedTemporaryFile
 from django.core.files.uploadedfile import SimpleUploadedFile
 
+THE_FILE = SimpleUploadedFile('test.png', 'a photo')
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -14,31 +16,31 @@ class UserFactory(factory.django.DjangoModelFactory):
     username = 'john'
 
 
-# class ImageFactory(factory.django.DjangoModelFactory):
-#     class Meta:
-#         model = Photo
+class PhotoTestCase(TestCase):
 
-#     image = factory.LazyAttribute(
-#             lambda _: ContentFile(
-#                 factory.django.ImageField()._make_data(
-#                     {'width': 1024, 'height': 768}
-#                 ), 'example.jpg'
-#             )
-#         )
+    def setUp(self):
+        UserFactory()
+
+    def test_photo_has_user(self):
+        user_john = User.objects.get(username='john')
+        photo1 = Photo()
+        photo1.user = user_john
+        photo1.image = THE_FILE
+        photo1.save()
+        self.assertEquals(Photo.objects.all()[0].user, user_john) 
 
 
 class AlbumTestCase(TestCase):
-        def setUp(self):
-            UserFactory()
-            self.file = SimpleUploadedFile('test.png', 'a photo')
+    def setUp(self):
+        UserFactory()
 
-        def test_album(self):
-            photo1 = Photo()
-            photo1.user = User.objects.get(username='john')
-            photo1.image = self.file
-            album1 = Album()
-            album1.user = User.objects.get(username='john')
-            album1.save()
-            photo1.save()
-            photo1.albums.add(album1)
-            self.assertIn(photo1, album1.photos.all())
+    def test_photo_in_album(self):
+        photo1 = Photo()
+        photo1.user = User.objects.get(username='john')
+        photo1.image = THE_FILE
+        album1 = Album()
+        album1.user = User.objects.get(username='john')
+        album1.save()
+        photo1.save()
+        photo1.albums.add(album1)
+        self.assertIn(photo1, album1.photos.all())
