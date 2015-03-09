0aba58392244865395bbfe80fdf75c10a1f1340b
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-09 16:26:24 -0700

    uncomment registration route.

--- imager/urls.py
+++ imager/urls.py
@@ -9,8 +9,8 @@ urlpatterns = patterns('',
                        url(r'^$', 'imager.views.home'),
                        # url(r'^blog/', include('blog.urls')),
                        url(r'^admin/', include(admin.site.urls)),
-                       # url(r'^accounts/', include(
-                       #     'registration.backends.default.urls'))
+                       url(r'^accounts/', include(
+                           'registration.backends.default.urls'))
                        )
 
 if settings.DEBUG:
