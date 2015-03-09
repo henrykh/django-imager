7eeed5f09bce0b09d7a7a764dbc8e81764a7ebd5
henrykh <henryhowes@gmail.com>
2015-03-09 15:22:25 -0700

    adds test of image metadata

--- imager_images/tests.py
+++ imager_images/tests.py
@@ -2,8 +2,8 @@ import factory
 from django.test import TestCase
 from imager_images.models import Album, Photo
 from django.contrib.auth.models import User
-from tempfile import NamedTemporaryFile
 from django.core.files.uploadedfile import SimpleUploadedFile
+import datetime
 
 THE_FILE = SimpleUploadedFile('test.png', 'a photo')
 
@@ -29,26 +29,42 @@ class PhotoTestCase(TestCase):
         photo1.save()
         self.assertEquals(Photo.objects.all()[0].user, user_john)
 
+    def test_photo_metadata(self):
+        user_john = User.objects.get(username='john')
+        photo1 = Photo()
+        photo1.user = user_john
+        photo1.image = THE_FILE
+        photo1.title = "Image Title"
+        photo1.description = "An Image"
+        photo1.published = "pvt"
+        photo1.save()
+        the_photo = Photo.objects.all()[0]
+        self.assertEquals(the_photo.title, "Image Title")
+        self.assertEquals(the_photo.description, "An Image")
+        self.assertEquals(the_photo.published, "pvt")
+        self.assertEquals(the_photo.date_uploaded, datetime.date.today())
+        self.assertEquals(the_photo.date_modified, datetime.date.today())
+
 
 class AlbumTestCase(TestCase):
-        def setUp(self):
-            UserFactory()
-            self.file = SimpleUploadedFile('test.png', 'a photo')
+    def setUp(self):
+        UserFactory()
+        self.file = SimpleUploadedFile('test.png', 'a photo')
 
-        def test_album_exists(self):
-            user_john = User.objects.get(username='john')
-            album1 = Album()
-            album1.user = user_john
-            album1.save()
-            self.assertEqual(Album.objects.get(pk=1).user, user_john)
+    def test_album_exists(self):
+        user_john = User.objects.get(username='john')
+        album1 = Album()
+        album1.user = user_john
+        album1.save()
+        self.assertEqual(Album.objects.get(pk=1).user, user_john)
 
-        def test_photo_in_album(self):
-            photo1 = Photo()
-            photo1.user = User.objects.get(username='john')
-            photo1.image = THE_FILE
-            album1 = Album()
-            album1.user = User.objects.get(username='john')
-            album1.save()
-            photo1.save()
-            photo1.albums.add(album1)
-            self.assertIn(photo1, album1.photos.all())
+    def test_photo_in_album(self):
+        photo1 = Photo()
+        photo1.user = User.objects.get(username='john')
+        photo1.image = THE_FILE
+        album1 = Album()
+        album1.user = User.objects.get(username='john')
+        album1.save()
+        photo1.save()
+        photo1.albums.add(album1)
+        self.assertIn(photo1, album1.photos.all())
