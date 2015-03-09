2b9d0194db8129c8e9814fceb4b3bf2caf848378
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-09 14:43:59 -0700

    add exclude to forms

--- imager_images/tests.py
+++ imager_images/tests.py
@@ -19,7 +19,14 @@ class AlbumTestCase(TestCase):
             UserFactory()
             self.file = SimpleUploadedFile('test.png', 'a photo')
 
-        def test_album(self):
+        def test_album_exists(self):
+            user_john = User.objects.get(username='john')
+            album1 = Album()
+            album1.user = user_john
+            album1.save()
+            self.assertEqual(Album.objects.get(pk=1).user, user_john)
+
+        def test_photo_in_album(self):
             photo1 = Photo()
             photo1.user = User.objects.get(username='john')
             photo1.image = self.file
