addbb383efed8a9810ff5441540225667a2a2c63
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-09 15:13:19 -0700

    remove deprecated file_size method from album model.

--- imager_images/tests.py
+++ imager_images/tests.py
@@ -31,56 +31,58 @@ class PhotoTestCase(TestCase):
 
 
 class AlbumTestCase(TestCase):
-        def setUp(self):
-            UserFactory()
-            self.file = SimpleUploadedFile('test.png', 'a photo')
+    def setUp(self):
+        UserFactory()
+        UserFactory(username='jane')
+        self.file = SimpleUploadedFile('test.png', 'a photo')
 
-        def test_album_owner(self):
-            user_john = User.objects.get(username='john')
-            album1 = Album()
-            album1.user = user_john
-            album1.save()
-            self.assertEqual(Album.objects.get(pk=1).user, user_john)
+    def test_album_owner(self):
+        user_john = User.objects.get(username='john')
+        album1 = Album()
+        album1.user = user_john
+        album1.save()
+        self.assertEqual(Album.objects.get(pk=1).user, user_john)
 
-        def test_photo_in_album(self):
-            user_john = User.objects.get(username='john')
-            photo1 = Photo()
-            photo1.user = user_john
-            photo1.image = THE_FILE
-            album1 = Album()
-            album1.user = user_john
-            album1.save()
-            photo1.save()
-            photo1.albums.add(album1)
-            self.assertIn(photo1, album1.photos.all())
+    def test_photo_in_album(self):
+        user_john = User.objects.get(username='john')
+        photo1 = Photo()
+        photo1.user = user_john
+        photo1.image = THE_FILE
+        album1 = Album()
+        album1.user = user_john
+        album1.save()
+        photo1.save()
+        photo1.albums.add(album1)
+        self.assertIn(photo1, album1.photos.all())
 
-        def test_photos_in_album(self):
-            user_john = User.objects.get(username='john')
-            photo1 = Photo()
-            photo1.user = user_john
-            photo1.image = THE_FILE
-            photo2 = Photo()
-            photo2.user = user_john
-            photo2.image = THE_FILE
-            album1 = Album()
-            album1.user = user_john
-            album1.save()
-            photo1.save()
-            photo2.save()
-            photo1.albums.add(album1)
-            photo2.albums.add(album1)
-            self.assertIn(photo1, album1.photos.all())
-            self.assertIn(photo2, album1.photos.all())
+    def test_photos_in_album(self):
+        user_john = User.objects.get(username='john')
+        photo1 = Photo()
+        photo1.user = user_john
+        photo1.image = THE_FILE
+        photo2 = Photo()
+        photo2.user = user_john
+        photo2.image = THE_FILE
+        album1 = Album()
+        album1.user = user_john
+        album1.save()
+        photo1.save()
+        photo2.save()
+        photo1.albums.add(album1)
+        photo2.albums.add(album1)
+        self.assertIn(photo1, album1.photos.all())
+        self.assertIn(photo2, album1.photos.all())
 
-        def test_photo_not_owned_by_album_owner_not_addable(self):
-            user_john = User.objects.get(username='john')
-            user_jane = User.objects.get(username='jane')
-            photo1 = Photo()
-            photo1.user = user_jane
-            photo1.image = THE_FILE
-            album1 = Album()
-            album1.user = user_john
-            album1.save()
-            photo1.save()
-            photo1.albums.add(album1)
-            self.assertIn(photo1, album1.photos.all())
+    def test_photo_not_owned_by_album_owner_not_addable(self):
+        user_john = User.objects.get(username='john')
+        import pdb; pdb.set_trace()
+        user_jane = User.objects.get(username='jane')
+        photo1 = Photo()
+        photo1.user = user_jane
+        photo1.image = THE_FILE
+        album1 = Album()
+        album1.user = user_john
+        album1.save()
+        photo1.save()
+        photo1.albums.add(album1)
+        self.assertIn(photo1, album1.photos.all())
