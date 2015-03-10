39a3df466b3ff417587e12dd8fcc30e825258fb0
henrykh <henryhowes@gmail.com>
2015-03-10 14:28:10 -0700

    moves profile.html

---
+++ imager_user/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import patterns, url
+
+
+urlpatterns = patterns('',
+                       # Examples:
+                       url(r'^$', 'imager_user.views.profile', name='profile'),
+                       )
