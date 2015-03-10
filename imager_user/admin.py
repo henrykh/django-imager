994d5b0ea12b3d250a8224dd6c49de602d64d2ac
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-10 13:16:41 -0700

    modify profile admin to account for new or edit profile and update gitignore to ignore sublime workspace and project files.

--- imager_user/admin.py
+++ imager_user/admin.py
@@ -11,6 +11,41 @@ class ProfileInline(admin.StackedInline):
 
 
 class ProfileAdmin(admin.ModelAdmin):
+    def get_fields(self, request, obj=None):
+        if obj:
+            return ('user',
+                    'follows',
+                    'blocking',
+                    'picture',
+                    'picture_privacy',
+                    'phone_number',
+                    'phone_privacy',
+                    'birthday',
+                    'birthday_privacy',
+                    'name_privacy',
+                    'email_privacy',
+                    )
+        else:
+            return ('user',
+                    'follows',
+                    'blocking',
+                    'picture',
+                    'picture_privacy',
+                    'phone_number',
+                    'phone_privacy',
+                    'birthday',
+                    'birthday_privacy',
+                    'name_privacy',
+                    'email_privacy',
+                    )
+
+    def get_readonly_fields(self, request, obj=None):
+        if obj:
+            return ('user',
+                    )
+        else:
+            return ()
+
     list_display = ('user',
                     'phone_number',
                     'birthday',
