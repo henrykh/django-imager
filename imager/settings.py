42c6d578b36038594e3b902df923d2a467a85cc9
Constantine Hatzis <constantine.hatzis@gmail.com>
2015-03-11 11:14:12 -0700

    implement django debug toolbar. pip install requirements

--- imager/settings.py
+++ imager/settings.py
@@ -29,6 +29,11 @@ ALLOWED_HOSTS = []
 
 SITE_ID = 1
 
+# IPs
+INTERNAL_IPS = ('127.0.0.1',
+                '::1'
+                )
+
 # Application definition
 
 INSTALLED_APPS = (
@@ -43,7 +48,8 @@ INSTALLED_APPS = (
     'imager_user',
     'imager_images',
     'sorl.thumbnail',
-    'registration'
+    'registration',
+    'debug_toolbar',
 )
 
 MIDDLEWARE_CLASSES = (
@@ -54,6 +60,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'debug_toolbar.middleware.DebugToolbarMiddleware',
 )
 
 ROOT_URLCONF = 'imager.urls'
@@ -116,3 +123,20 @@ REGISTRATION_AUTO_LOGIN = True
 REGISTRATION_OPEN = True
 LOGIN_URL = '/accounts/login/'
 LOGIN_REDIRECT_URL = '/profile/'
+
+# Debug toolbar settings
+DEBUG_TOOLBAR_PATCH_SETTINGS = False
+DEBUG_TOOLBAR_PANELS = (
+    'debug_toolbar.panels.versions.VersionsPanel',
+    'debug_toolbar.panels.timer.TimerPanel',
+    'debug_toolbar.panels.settings.SettingsPanel',
+    'debug_toolbar.panels.headers.HeadersPanel',
+    'debug_toolbar.panels.request.RequestPanel',
+    'debug_toolbar.panels.sql.SQLPanel',
+    'debug_toolbar.panels.staticfiles.StaticFilesPanel',
+    'debug_toolbar.panels.templates.TemplatesPanel',
+    'debug_toolbar.panels.cache.CachePanel',
+    'debug_toolbar.panels.signals.SignalsPanel',
+    'debug_toolbar.panels.logging.LoggingPanel',
+    'debug_toolbar.panels.redirects.RedirectsPanel',
+)
