247cfe976476c46c35c72ee0323f79f6bf06e198
henrykh <henryhowes@gmail.com>
2015-03-10 17:15:50 -0700

    initial work on stream view

--- imager_images/urls.py
+++ imager_images/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('',
                        # Examples:
                        url(r'library/',
                            'imager_images.views.library', name='library'),
+                       url(r'stream/', 'imager_images.views.stream', name='stream')
                        )
--- imager_images/views.py
+++ imager_images/views.py
@@ -1,9 +1,19 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
-
+from imager_images.models import Photo
 
 @login_required
 def library(request):
+    # import pdb; pdb.set_trace();
     context = {'albums': request.user.albums.all()}
-    # import pdb; pdb.set_trace()
     return render(request, 'library.html', context)
+
+
+@login_required
+def stream(request):
+    # import pdb; pdb.set_trace();
+    stream_users = [profile.user for profile in request.user.profile.following()]
+    stream_users.append(request.user)
+    context = {'photos': Photo.objects.filter(
+        user__in=stream_users).filter(published__in=['pub', 'shd']).order_by('date_published')}
+    return render(request, 'stream.html', context)
