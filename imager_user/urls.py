3e8b1c1a40ea2d3675faa6801e5142c0c48c3e74
henrykh <henryhowes@gmail.com>
2015-03-12 18:42:00 -0700

    work on profile edit

--- imager_user/urls.py
+++ imager_user/urls.py
@@ -4,4 +4,5 @@ from django.conf.urls import patterns, url
 urlpatterns = patterns('',
                        # Examples:
                        url(r'^$', 'imager_user.views.profile', name='profile'),
+                       url(r'^edit/$', 'imager_user.views.profile_edit', name='profile_edit')
                        )
--- imager_user/views.py
+++ imager_user/views.py
@@ -1,8 +1,38 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from imager_user.models import ImagerProfile
+from django.views.generic import UpdateView
 
 
 @login_required
 def profile(request):
     context = {'name': request.user}
     return render(request, 'profile.html', context)
+
+
+@login_required
+
+def profile_edit(request):
+    context = {'name': request.user}
+    return render(request, 'profile_edit.html', context)
+
+
+class ProfileEdit(UpdateView):
+    def user_passes_test(self, request):
+        if request.user.is_authenticated():
+            self.object = self.get_object()
+            return self.object.user == request.user
+        return False
+
+    def dispatch(self, request, *args, **kwargs):
+        if not self.user_passes_test(request):
+            return redirect_to_login(request.get_full_path())
+        return super(PhotoUpdateView, self).dispatch(
+            request, *args, **kwargs)
+
+    def get_success_url(self):
+        return self.request.GET['src']
+
+    form_class = PhotoUpdateViewForm
+    model = ImagerProfile
+    template_name = 'profile_edit.html'
